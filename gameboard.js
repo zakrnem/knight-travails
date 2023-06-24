c546aa4243c4df34a1163e9f91e4b8405b7be1c2
zakrnem <zakrnem@zohomail.com>
2023-06-23 22:06:39 -0600

    Insert knight function working

--- gameboard.js
+++ gameboard.js
@@ -13,12 +13,31 @@ function gameboard() {
     }
   }
   return chessboard
+  //Implement pretty print
+}
+
+function knight(positionX, positionY) {
+  return {
+    positionX: positionX,
+    positionY: positionY,
+
+    getPosition: function () {
+      return `Current position: ` + positionX + positionY
+    },
+  }
+}
+
+function insertKnight(board, letterCoordinate, numberCoordinate) {
+  board[numberCoordinate][letterCoordinate].push("knight")
+  return knight(letterCoordinate, numberCoordinate)
 }
 
 function driver() {
-    const board = gameboard()
-    board[1].A.push('knight')
-    console.log(board)
+  const board = gameboard()
+  const createdKnight = insertKnight(board, "D", 5)
+  console.log(createdKnight.getPosition())
+  //board[7]['H'].push('knight')
+  console.log(board)
 }
 
 module.exports = {
